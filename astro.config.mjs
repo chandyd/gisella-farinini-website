48e6fe831260244c84c7c87d0f95e86b64d73d1b
Roberto Calvo <robycalvo@gmail.com>
2026-03-31 06:25:31 +0800

    Deploy museum-quality v2.0 - Tue Mar 31 06:25:31 AM CST 2026

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-
-export default defineConfig({
-  integrations: [react()],
-  site: 'https://gisellafarinini.com',
-  output: 'static',
-});
